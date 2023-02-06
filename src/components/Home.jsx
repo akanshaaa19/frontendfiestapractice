a849c0552ea99b10a1529d937eb363cea4030890
Nishika Yadav <nishikayadav26@gmail.com>
2023-02-06 23:11:06 +0530

    updates in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
-function Home (){
+function Home() {
    return <section>
-    
+      <div className="row">
+         <div className="col">
+            <h1>Make reliable agricultural decisions with AI</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+         </div>
+      </div>
    </section>;
 }
- 
+
 export default Home
